e9773838510d6f616b7f7a893c16ed4a7ad7b102
titikkoma88 <jie.koma88@gmail.com>
2021-11-07 18:18:24 +0700

    Last Commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row header mb-5 p-3">
+        <div class="col-lg-12">
+          <h2>What I do Today?</h2>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row content">
+        <div class="col-lg-12">
+          <div class="pekerjaan-rumah">
+            <div
+              v-for="tugasSaya in tugas"
+              v-bind:key="tugasSaya.id"
+              class="tugas"
+            >
+              <p>{{ tugasSaya.namaTugas }}</p>
+              <div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+  <label class="form-check-label" for="flexCheckDefault">
+    Default checkbox
+  </label>
+</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function() {
+    return {
+      tugas: [
+        {
+          id: 1,
+          namaTugas: "Mencuci baju",
+          isDone: false,
+        },
+        {
+          id: 2,
+          namaTugas: "Ngoding Website",
+          isDone: false,
+        },
+        {
+          id: 3,
+          namaTugas: "Belanja Kebutuhan",
+          isDone: false,
+        },
+        {
+          id: 4,
+          namaTugas: "Istirahat 8 Jam",
+          isDone: false,
+        },
+      ],
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
@@ -24,5 +72,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.header {
+  background: #1abc9c;
+}
+.header h2 {
+  margin: 0;
 }
 </style>
